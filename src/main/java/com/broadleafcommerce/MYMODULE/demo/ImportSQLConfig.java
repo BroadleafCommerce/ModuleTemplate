370e84828696dad87cf4e49e9b82634dd2ec5cfe
Chris Kittrell <ckittrell@broadleafcommerce.com>
2017-03-16 14:23:12 -0500

    BroadleafCommerce/QA#2681 - Reorganize applicationContexts & refactor bean configurations to use the new java-based configuration patterns

---
+++ src/main/java/com/broadleafcommerce/MYMODULE/demo/ImportSQLConfig.java
@@ -0,0 +1,52 @@
+/*-
+ * #%L
+ * BroadleafCommerce MYMODULE
+ * %%
+ * Copyright (C) 2009 - 2017 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt).
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * 
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Broadleaf Commerce, LLC
+ * The intellectual and technical concepts contained
+ * herein are proprietary to Broadleaf Commerce, LLC
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Broadleaf Commerce, LLC.
+ * #L%
+ */
+package com.broadleafcommerce.MYMODULE.demo;
+
+import org.broadleafcommerce.common.demo.AutoImportPersistenceUnit;
+import org.broadleafcommerce.common.demo.AutoImportSql;
+import org.broadleafcommerce.common.demo.AutoImportStage;
+import org.broadleafcommerce.common.demo.DemoCondition;
+import org.broadleafcommerce.common.demo.ImportCondition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author
+ */
+@Configuration("blMYMODULEData")
+@Conditional(ImportCondition.class)
+public class ImportSQLConfig {
+
+    @Bean
+    public AutoImportSql blMYMODULESecurity() {
+        return new AutoImportSql(AutoImportPersistenceUnit.BL_PU, "config/bc/sql/load_MYMODULE_admin_security.sql", AutoImportStage.PRIMARY_MODULE_SECURITY);
+    }
+
+    @Bean
+    @Conditional(DemoCondition.class)
+    public AutoImportSql blMYMODULEDemoData() {
+        return new AutoImportSql(AutoImportPersistenceUnit.BL_PU,"config/bc/sql/samples/load_MYMODULE_demo_data.sql", AutoImportStage.PRIMARY_BASIC_DATA);
+    }
+}
